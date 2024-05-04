74eece58e82e3c038e493e9fcf56b6216daa1ab8
Enrique Puertas <puertasman@gmail.com>
2024-05-04 08:57:20 +0200

    empiezan los archivos en el 61

---
+++ 006.py
@@ -0,0 +1,6 @@
+d = 5
+if d >= 0:
+    print("Positiva")
+else:
+    print("Negativa")
+    
--- 061.py
+++ 061.py
@@ -1,1 +1,6 @@
-# está vacío
+def leerFichero(rutaFichero):
+    with open(rutaFichero, 'r') as f:
+        contenido = f.read()
+        print(contenido)
+    
+leerFichero("prueba.txt")
