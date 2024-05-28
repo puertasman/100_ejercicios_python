8624e11297cbe4c26a0d28f590e972c5e2aef631
Enrique Puertas <puertasman@gmail.com>
2024-05-28 15:26:32 +0200

    hasta el 99

---
+++ 100.py
@@ -0,0 +1,9 @@
+def sumaMayor(L):
+    total = 0
+    for i in range(len(L)):
+        total += sum(L[i:])
+    return total
+
+print(sumaMayor([-8,-4,6,8,-6,10,-4,-4]))
+
+print(sumaMayor([-6,1,8,-7,1,9,-1,2]))
